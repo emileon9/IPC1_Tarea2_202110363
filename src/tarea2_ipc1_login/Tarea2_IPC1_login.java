3029b6d67a19be446a05c844ec1b3fe1f718942b
emily <angeline.dl09@gmail.com>
2022-08-24 18:15:19 -0400

    todo

---
+++ src/tarea2_ipc1_login/Tarea2_IPC1_login.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2_ipc1_login;
+
+/**
+ *
+ * @author Mily Foreveralone
+ */
+public class Tarea2_IPC1_login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/usuarios.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mily Foreveralone
+ */
+public class usuarios {
+    
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+    private String apellido;
+    private int carnet;
+    
+    
+     usuarios (String nombre, String apellido, int carnet){
+         this.nombre=nombre;
+         this.apellido=apellido;
+         this.carnet=carnet;
+     }
+    
+    
+}
